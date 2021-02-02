31551a2eec020718512ed2e040abef13f8c8d46d
milon27 <mdjahidulislammilon@gmail.com>
2021-02-03 00:53:26 +0600

    first progress release after user  auth api

--- api/utils/Define.js
+++ api/utils/Define.js
@@ -1,11 +1,7 @@
 const Define = {
-    part_collection: 'part_collection',
     user_collection: 'user_collection',
-    order_collection: 'order_collection',
-    order_sub_collection: 'order_sub_collection',
     S_ADMIN: "S_ADMIN",
-    ADMIN: "ADMIN",
-    USER: "USER",
+    ADMIN: "ADMIN"
 }
 
 module.exports = Define
--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-const { https } = require('firebase-functions')
-
 require('dotenv').config()
 const express = require('express')
 const app = express()
@@ -18,14 +16,10 @@ app.get('/', (req, res) => {
 })
 
 //crud operation, get-data,update-data,add-data
-app.use('/api/v1', require('./api/router/dataRouter'))
 app.use('/api/auth', require('./api/router/authRouter'))
-app.use('/api/order', require('./api/router/orderRouter'))
 
 
-exports.inventoryApi = https.onRequest(app)
-
-// const port = process.env.PORT || 2828;
-// app.listen(port, () => {
-//     console.log(`server running on port ${port}`);
-// })
+const port = process.env.PORT || 2828;
+app.listen(port, () => {
+    console.log(`server running on port ${port}`);
+})
