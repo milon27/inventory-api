cb90e81bc4ded5d6a3a253390c3cfbac6c047826
milon27 <mdjahidulislammilon@gmail.com>
2021-02-03 09:32:53 +0600

    added comment

--- api/router/authRouter.js
+++ api/router/authRouter.js
@@ -2,8 +2,11 @@ const express = require('express');
 const { CreateUserController, LoginUserController, DeleteUserController } = require('../controller/authController');
 const router = express.Router();
 
+//localhost:2727/api/auth/create-user
 router.post('/create-user', CreateUserController)
+//http://localhost:2727/api/auth/login-user
 router.post('/login-user', LoginUserController)
+//http://localhost:2727/api/auth/delete-user/0YBIMc6dDuXPYNGFKtCbF26whCW2
 router.delete('/delete-user/:uid', DeleteUserController)
 
 module.exports = router
